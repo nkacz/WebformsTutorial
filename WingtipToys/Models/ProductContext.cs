ae320ed58ec9294f05cd581c5899ae74b38bd5d8
nkaczmarek <nkaczmarek@caleres.com>
2015-11-23 10:32:15 -0600

    Displayed data, changed styling, added assets

--- WingtipToys/Models/ProductContext.cs
+++ WingtipToys/Models/ProductContext.cs
@@ -8,10 +8,7 @@ namespace WingtipToys.Models
 {
     public class ProductContext : DbContext
     {
-        public ProductContext()
-            : base("WingtipToys")
-        {
-        }
+        public ProductContext() : base("WingtipToys") { }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
     }
