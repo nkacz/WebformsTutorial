f836fcbe4eb8e7b71ecd17d860eb1353ab6fb027
nkaczmarek <nkaczmarek@caleres.com>
2015-11-23 15:59:25 -0600

    Added routing for categories and products

--- WingtipToys/Global.asax.cs
+++ WingtipToys/Global.asax.cs
@@ -26,6 +26,23 @@ namespace WingtipToys
             // Create the custom role and user.
             RoleActions roleActions = new RoleActions();
             roleActions.AddUserAndRole();
+
+                 // Add Routes.
+            RegisterCustomRoutes(RouteTable.Routes);
         }
+
+        void RegisterCustomRoutes(RouteCollection routes)
+        {
+            routes.MapPageRoute(
+                "ProductsByCategoryRoute",
+                "Category/{categoryName}",
+                "~/ProductList.aspx"
+            );
+            routes.MapPageRoute(
+                "ProductByNameRoute",
+                "Product/{productName}",
+                "~/ProductDetails.aspx"
+            );
+        }   
     }
 }
